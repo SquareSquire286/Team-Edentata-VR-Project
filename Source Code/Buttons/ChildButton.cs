7745e42795ece727eaad8d604a8da9fa3ee0e26d
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-10-21 19:33:25 -0600

    Add files via upload

---
+++ Source Code/Buttons/ChildButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildButton : AbstractButton
+{
+    private bool stoppedLastTime;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        stoppedLastTime = true;
+        isPressed = false;
+        transform.localScale = releasedPosition;
+    }
+
+    public override void OnPress()
+    {
+        transform.localScale = pressedPosition;
+
+        if (stoppedLastTime)
+            affectedObject.ExecuteEvent();
+
+        else affectedObject.StopEvent();
+
+        stoppedLastTime = !stoppedLastTime;
+    }
+
+    public override void OnRelease()
+    {
+        transform.localScale = releasedPosition;
+    }
+}
