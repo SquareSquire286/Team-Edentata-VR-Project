adb7a90c53118d2932df8bf115ac036e6d91d85b
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-11-18 21:43:20 -0600

    Add files via upload

---
+++ Source Code/Helpers/Handle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//*********************************
+// Purpose: Used to maneuver doors
+//*********************************
+public class Handle : MonoBehaviour
+{
+    public Transform target; // set to the invisible handle
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.MovePosition(target.transform.position); 
+    }
+}
