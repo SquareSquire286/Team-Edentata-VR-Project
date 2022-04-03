941c33b15f7c68e53d78f168cd3fcbf431848934
Jacobchapman99 <55856533+Jacobchapman99@users.noreply.github.com>
2022-04-03 15:15:24 -0600

    comments

--- Scripts/Button Events/TextButtonEvent.cs
+++ Scripts/Button Events/TextButtonEvent.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// *************************************************************************************
+// Purpose: 
+//
+// Class Variables: 
+//          text ->
+//                
+//          
+// *************************************************************************************
 public class TextButtonEvent : AbstractButtonEvent
 {
     private Text text;
 
-    // Start is called before the first frame update
+
+    // ****************************************************************************
+    // Functionality: Start is called before the first frame update. 
+    // 
+    // Parameters: none
+    // Return: none
+    // *****************************************************************************
     public override void Start()
     {
         text = GetComponent<Text>();
         text.enabled = false;
     }
 
+
+    // ****************************************************************************
+    // Functionality: 
+    // 
+    // Parameters: none
+    // Return: none
+    // *****************************************************************************
     public override void ExecuteEvent()
     {
         text.enabled = true;
--- Scripts/Button Events/VideoButtonEvent.cs
+++ Scripts/Button Events/VideoButtonEvent.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
+// *************************************************************************************
+// Purpose: 
+//
+// Class Variables: 
+//          videoPlayer ->
+//                
+//          
+// *************************************************************************************
 public class VideoButtonEvent : AbstractButtonEvent
 {
     private VideoPlayer videoPlayer;
 
-    // Start is called before the first frame update
+
+    // ****************************************************************************
+    // Functionality: Start is called before the first frame update. Ensures 
+    //                the video component gets attached to the video source object.
+    // 
+    // Parameters: none
+    // Return: none
+    // *****************************************************************************
     public override void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
     }
 
+
+    // ****************************************************************************
+    // Functionality: 
+    // 
+    // Parameters: none
+    // Return: none
+    // *****************************************************************************
     public override void ExecuteEvent()
     {
         if (videoPlayer.isPlaying)
@@ -21,6 +43,14 @@ public class VideoButtonEvent : AbstractButtonEvent
         else videoPlayer.Play();
     }
 
+
+
+    // ****************************************************************************
+    // Functionality: 
+    // 
+    // Parameters: none
+    // Return: none
+    // *****************************************************************************
     public override void StopEvent()
     {
         videoPlayer.Pause();
