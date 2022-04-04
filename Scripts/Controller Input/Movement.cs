083a4a7624b408883e2de73194ce91df753a326d
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2022-04-04 14:03:06 -0600

    Still need to finalize formatting

--- Scripts/Controller Input/Movement.cs
+++ Scripts/Controller Input/Movement.cs
@@ -1,30 +1,40 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// **************************************************************
+// Purpose: This class implements the camera movement module via
+//          the left joystick. It listens for a 2D vector of
+//          floating-point numbers between -1.0 and 1.0, which 
+//          represent the joystick's deviation from the default 
+//          position in both horizontal and vertical directions, 
+//          and applies this vector to the camera's rigidbody motion.
+//***************************************************************
 public class Movement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 2.0f;
     public GameObject camera;
+    private Rigidbody cameraRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 2.0f;
+        cameraRigidbody = camera.GetComponent<Rigidbody>(); // initialize the camera rigidbody
     }
 
     // Update is called once per frame
     void Update()
     {
         var joystick = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick); // "joystick" is a 2D Vector (x, y)
-        float fixedY = camera.GetComponent<Rigidbody>().position.y; // camera.transform is a 3D Vector (x, y, z), and y should never change
+        float fixedY = cameraRigidbody.position.y; // cameraRigidbody.position is a 3D Vector (x, y, z), and y should never change
 
-        camera.GetComponent<Rigidbody>().position += (transform.right * joystick.x + transform.forward * joystick.y) * speed * Time.deltaTime;
-        camera.GetComponent<Rigidbody>().position = new Vector3(camera.GetComponent<Rigidbody>().position.x, fixedY, camera.GetComponent<Rigidbody>().position.z);
+        // apply joystick.x to the camera rigidbody's sway (X) axis, and joystick.y to the camera rigidbody's surge (Z) axis
+        cameraRigidbody.position += (transform.right * joystick.x + transform.forward * joystick.y) * speed * Time.deltaTime;
+        cameraRigidbody.position = new Vector3(cameraRigidbody.position.x, fixedY, cameraRigidbody.position.z);
 
-        if(joystick.x == 0 && joystick.y == 0)
+        if (joystick.x == 0 && joystick.y == 0) // if no input is detected from the left joystick in either direction, zero the velocity vector
         {
-            camera.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            cameraRigidbody.velocity = new Vector3(0, 0, 0);
         }
     }
 }
