26a6ba4b37ce3611f4bed4635d6585db814c6fe6
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2022-04-04 13:18:38 -0600

    Finished commenting

--- Scripts/Buttons/ChildButton.cs
+++ Scripts/Buttons/ChildButton.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 // *************************************************************************************
-// Purpose: 
+// Purpose: A concrete subclass of AbstractButton that applies to the button on the remote
+//          object in Room 2.
 //
 // Class Variables: 
-//          stoppedLastTime ->
-//          roomController ->
+//          stoppedLastTime -> a Boolean condition that stores the previous state of the video
+//          roomController -> instance of RoomController script that facilitates room
+//                            parameter logic
+//          All class variables from AbstractButton are inherited.
+//          The AbstractButtonEvent variable must be initialized to a VideoButtonEvent in
+//          the Unity Inspector window (i.e. the spider video player in Room 2).
 //                
 //          
 // *************************************************************************************
@@ -16,10 +21,10 @@ public class ChildButton : AbstractButton
     private bool stoppedLastTime;
     public RoomController roomController;
 
-
-
     // *************************************************************
-    // Functionality: Start is called before the first frame update 
+    // Functionality: Start is called before the first frame update.
+    //                Initializes the two boolean conditions and the
+    //                button's starting position.
     //                                                              
     // Parameters: none                                             
     // Return: none                                                 
@@ -34,14 +39,16 @@ public class ChildButton : AbstractButton
 
     
     // ****************************************************************************
-    // Functionality: Button is pressed
+    // Functionality: Called when the button is pressed. Updates the button's
+    //                position, communicates to the RoomController field that the
+    //                condition has been fulfilled, and toggles the status of the
+    //                AbstractButtonEvent based on the previous state.
     // 
     // Parameters: none
     // Return: none
     // ****************************************************************************
     public override void OnPress()
     {
-        Debug.Log("Stopped last time: " + stoppedLastTime);
         transform.localScale = pressedPosition;
         roomController.UpdateRoomConditions(this.gameObject);
 
@@ -55,7 +62,7 @@ public class ChildButton : AbstractButton
 
 
     // ****************************************************************************
-    // Functionality: Button is released
+    // Functionality: Button is released to its default position.
     // 
     // Parameters: none
     // Return: none
