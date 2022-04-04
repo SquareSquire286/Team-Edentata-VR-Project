dd3efe4e1627deb23022e51ace03cd1d56e9b608
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2022-04-04 13:00:51 -0600

    Finished commenting

--- Scripts/Button Events/VideoButtonEvent.cs
+++ Scripts/Button Events/VideoButtonEvent.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using UnityEngine.Video;
 
 // *************************************************************************************
-// Purpose: 
+// Purpose: An event type that toggles the play status of a video on button press.
 //
 // Class Variables: 
-//          videoPlayer ->
+//          videoPlayer -> The video player containing the clip that will be played.
 //                
 //          
 // *************************************************************************************
@@ -30,7 +30,10 @@ public class VideoButtonEvent : AbstractButtonEvent
 
 
     // ****************************************************************************
-    // Functionality: 
+    // Functionality: Called on the first frame that the corresponding button is
+    //                pressed if the last function to be called on button press
+    //                was StopEvent. Pauses the video if it is currently playing,
+    //                or plays it if it is currently paused.
     // 
     // Parameters: none
     // Return: none
@@ -46,7 +49,9 @@ public class VideoButtonEvent : AbstractButtonEvent
 
 
     // ****************************************************************************
-    // Functionality: 
+    // Functionality: Called on the first frame that the corresponding button is
+    //                pressed if the last function to be called on button press
+    //                was ExecuteEvent. Pauses the video.
     // 
     // Parameters: none
     // Return: none
